4351fe3dceb178ad200b03c9ed641a0da113fac2
phantanloc1988 <phantanloc1988@gmail.com>
2020-06-07 11:42:04 +0700

    - Demo Valydation Jquery trong Root DemoJqueryVali.html - bai tap 2.2-Lab08: HomeCtroller/Validate (jQuery validation)

---
+++ buoi09/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using buoi09.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace buoi09.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public string TaoMaBaoMat()
+        {
+            //Sinh ra mã ngẫu nhiên
+            Random rd = new Random();
+            var maNgauNhien = rd.Next(1000, 10000).ToString();
+            //luu mã ngẫu nhiên vô Session
+            HttpContext.Session.SetString("maBaoMat", maNgauNhien.ToString());
+            return maNgauNhien;
+        }
+
+        public IActionResult Validate()
+        {
+            ViewBag.MaNgauNhien = TaoMaBaoMat();
+
+            return View();
+        }
+        public string KiemTraMaBaoMat(string MaBaoMat)
+        {
+            var mabaomatServer = HttpContext.Session.GetString("maBaoMat");
+            
+            return mabaomatServer.Equals(MaBaoMat) ? "true" : "false";
+        }
+
+        public string RefreshMaBaoMat()
+        {
+            return TaoMaBaoMat();
+        }
+    }
+}
