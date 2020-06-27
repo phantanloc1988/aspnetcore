788b09ac90f9f8b9914bd3c6907a90cf3f510a53
phantanloc1988 <phantanloc1988@gmail.com>
2020-06-27 17:19:36 +0700

    Buoi 14 (Lab-14) (connectionstrings.com; EFCorePowerTool tu generate DBFirst) - cai Entityfarmworkcore.SqlServer, Entityfarmworkcore.tool - Code First(Du an moi bat dau, DB chưa có tren SQLServer)   . Tao Model   . Tạo Class Lấy List data   . Tạo Connectionstrings trong appsetting;   . đăng ký dùng connection trong Starup   . Migration (tool - Nuget Package...) -> Nhập lệnh Add-Migration Name -> Update-Database  .hàm Include(KHóa ngoại)

---
+++ Buoi14-DatabaseFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi14_DatabaseFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ buoi14/Models/Loai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace buoi14.Models
+{
+    [Table("Loai")]
+    public class Loai
+    {
+        [Key]
+        public int MaLoai { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+
+        public string MoTa { get; set; }
+
+        [MaxLength(50)]
+        public string Hinh { get; set; }
+
+        // Để lấy dữ liệu hàng hóa khi có MaLoai không cần dùng Linq
+        public ICollection<HangHoa> HangHoa { get; set; }
+    }
+}
